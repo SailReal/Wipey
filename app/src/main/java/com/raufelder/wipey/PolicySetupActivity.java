028adbbdc3dd9c8700d51956eff50504ba99110c
Julian Raufelder <milestone@posteo.de>
2017-03-03 01:44:34 +0100

    Using Butter Knife

--- app/src/main/java/com/raufelder/wipey/PolicySetupActivity.java
+++ app/src/main/java/com/raufelder/wipey/PolicySetupActivity.java
@@ -12,23 +12,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PolicySetupActivity extends Activity {
     private static final int REQ_ACTIVATE_DEVICE_ADMIN = 10;
 
-    DevicePolicyManager devicePolicyManager;
-    private ComponentName mPolicyAdmin;
-    private Context context;
+    private DevicePolicyManager devicePolicyManager;
+    private ComponentName policyAdmin;
 
-    private int mCurrentScreenId;
+    private int currentScreenId;
     private int amountFailedPasswordsForWipe = 10; /* default */
+
+    private @BindView(R2.id.action_button) Button actionButton;
+    private @BindView(R2.id.amount_failed_passwords_for_wipe) EditText amountFailedPasswordsForWipeInputField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        context = getApplicationContext();
+        Context context = getApplicationContext();
 
-        mPolicyAdmin = new ComponentName(context, PolicyAdmin.class);
+        policyAdmin = new ComponentName(context, PolicyAdmin.class);
         devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
     }
 
@@ -49,8 +55,8 @@ public class PolicySetupActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQ_ACTIVATE_DEVICE_ADMIN && resultCode == RESULT_OK) {
-            devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-            devicePolicyManager.setMaximumFailedPasswordsForWipe(mPolicyAdmin, amountFailedPasswordsForWipe);
+            devicePolicyManager = (DevicePolicyManager) getApplicationContext().getSystemService(Context.DEVICE_POLICY_SERVICE);
+            devicePolicyManager.setMaximumFailedPasswordsForWipe(policyAdmin, amountFailedPasswordsForWipe);
 
             Toast.makeText(this, getString(R.string.successfully_activated, amountFailedPasswordsForWipe), Toast.LENGTH_SHORT).show();
         } else {
@@ -59,44 +65,31 @@ public class PolicySetupActivity extends Activity {
     }
 
     private void initPolicySetupScreen() {
-        EditText amountFailedPasswordsForWipeInputField = (EditText) findViewById(R.id.policy_password_length);
         amountFailedPasswordsForWipeInputField.setText(String.valueOf(amountFailedPasswordsForWipe));
     }
 
-    private View.OnClickListener mActivateButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent activateDeviceAdminIntent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-            activateDeviceAdminIntent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mPolicyAdmin);
+    @OnClick(R2.id.action_button)
+    public void submitButton(View view) {
+        Intent activateDeviceAdminIntent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        activateDeviceAdminIntent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, policyAdmin);
 
-            activateDeviceAdminIntent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, getResources().getString(R.string.device_admin_activation_message));
-            startActivityForResult(activateDeviceAdminIntent, REQ_ACTIVATE_DEVICE_ADMIN);
-        }
-    };
-
-    private void setupNavigation(View.OnClickListener listener) {
-        Button actionBtn = (Button) findViewById(R.id.setup_action_btn);
-        actionBtn.setOnClickListener(listener);
+        activateDeviceAdminIntent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, getResources().getString(R.string.device_admin_activation_message));
+        startActivityForResult(activateDeviceAdminIntent, REQ_ACTIVATE_DEVICE_ADMIN);
     }
 
     private void setScreenContent(final int screenId) {
-        mCurrentScreenId = screenId;
-        setContentView(mCurrentScreenId);
+        currentScreenId = screenId;
+        setContentView(currentScreenId);
+        ButterKnife.bind(this);
 
-        if(!devicePolicyManager.isAdminActive(mPolicyAdmin)) {
+        if(!devicePolicyManager.isAdminActive(policyAdmin)) {
             initPolicySetupScreen();
-            setupNavigation(mActivateButtonListener);
         }
-        else {
-            //Toast.makeText(this, "Already ", Toast.LENGTH_SHORT).show();
-        }
+        /*else {
+            // TODO change layout
+        }*/
     }
 
-    public static class PolicyAdmin extends DeviceAdminReceiver {
-        @Override
-        public void onDisabled(Context context, Intent intent) {
-            super.onDisabled(context, intent);
-        }
-    }
+    public static class PolicyAdmin extends DeviceAdminReceiver {}
 }
 
