228caec89e4d089e006841ab4fddf8ba3ccf1e10
Julian Raufelder <Julian@Raufelder.com>
2018-02-23 02:07:08 +0100

    Fixed some codacy warnings

--- app/src/main/java/com/raufelder/wipey/Preferences.java
+++ app/src/main/java/com/raufelder/wipey/Preferences.java
@@ -9,27 +9,27 @@ class Preferences {
 	private static final String WIPE_DEVICE = "WIPE_DEVICE";
 	private static final String AMOUNT_FAILED_LOGIN = "AMOUNT_FAILED_LOGIN";
 
-	static boolean isWipeActivated(Context context) {
+	protected static boolean isWipeActivated(Context context) {
 		SharedPreferences sharedPref = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 		return sharedPref.getBoolean(WIPE_DEVICE, true);
 	}
 
-	static void saveWipe(Context context) {
+	protected static void saveWipe(Context context) {
 		SharedPreferences sharedPref = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 		sharedPref.edit().putBoolean(WIPE_DEVICE, true).apply();
 	}
 
-	static void saveRestart(Context context) {
+	protected static void saveRestart(Context context) {
 		SharedPreferences sharedPref = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 		sharedPref.edit().putBoolean(WIPE_DEVICE, false).apply();
 	}
 
-	static int amountOfFailedLogin(Context context) {
+	protected static int amountOfFailedLogin(Context context) {
 		SharedPreferences sharedPref = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 		return sharedPref.getInt(AMOUNT_FAILED_LOGIN, 10);
 	}
 
-	static void setAmountOfFailedLogin(Context context, int amount) {
+	protected static void setAmountOfFailedLogin(Context context, int amount) {
 		SharedPreferences sharedPref = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 		sharedPref.edit().putInt(AMOUNT_FAILED_LOGIN, amount).apply();
 	}
